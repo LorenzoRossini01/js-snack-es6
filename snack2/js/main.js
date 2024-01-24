4056713ff4d23aade5492715ac40e2feea7da263
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-24 16:38:59 +0100

    feat: svolgimento in js snack 2

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -1,6 +1,25 @@
-console.log("js ok");
+const students = [
+  { id: 213, name: "Marco della Rovere", grades: 78 },
+  { id: 110, name: "Paola Cortellessa", grades: 96 },
+  { id: 250, name: "Andrea Mantegna", grades: 48 },
+  { id: 145, name: "Gaia Borromini", grades: 74 },
+  { id: 196, name: "Luigi Grimaldello", grades: 68 },
+  { id: 102, name: "Piero della Francesca", grades: 50 },
+  { id: 120, name: "Francesca da Polenta", grades: 84 },
+];
 
-const resultElement = document.getElementById("results");
+console.table(students);
 
-getRandomNumber(1, 20);
-console.log(randomNumber);
+const studentsUppercase = students.map((name, index) => ({
+  id: students[index].id,
+  name: students[index].name.toUpperCase(),
+  grades: students[index].grades,
+}));
+
+console.table(studentsUppercase);
+
+const students70 = studentsUppercase.filter((student) => student.grades >= 70);
+console.table(students70);
+
+const students70120 = students70.filter((student) => student.id >= 120);
+console.table(students70120);
