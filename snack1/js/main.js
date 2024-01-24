4fb5ddffd011c2999ea9e163f8fb46e8ddb1cf52
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-24 16:12:38 +0100

    feat: svolgimento in js

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,6 +1,41 @@
-console.log("js ok");
+const vips = [
+  "Dwayne Johnson",
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
 
-const resultElement = document.getElementById("results");
+const tableName = "Tavolo VIP";
 
-getRandomNumber(1, 20);
-console.log(randomNumber);
+console.table(vips);
+
+const Randomizer = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomUniqueNumber = (min, max, elementNumber) => {
+  const postiArray = [];
+  while (postiArray.length < elementNumber) {
+    let myRandomNum = Randomizer(1, elementNumber);
+    if (!postiArray.includes(myRandomNum)) {
+      postiArray.push(myRandomNum);
+    }
+  }
+  return postiArray;
+};
+
+const vipPosto = getRandomUniqueNumber(1, 11, 11);
+
+let vipsTable = [];
+vipsTable = vips.map((vip, index) => ({
+  Tavolo: tableName,
+  Vip: vip,
+  Posto: vipPosto[index],
+}));
+console.table(vipsTable);
