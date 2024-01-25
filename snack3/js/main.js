a0336bc0816234e4ba6af9030fdaa8aeba65566b
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-25 14:45:26 +0100

    feat: add result in dom

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,7 +1,9 @@
+const cardContainerEl = document.getElementById("card-container");
+
 const bicycleArray = [
-  { name: "Bici1", weight: 8 },
-  { name: "Bici2", weight: 7 },
-  { name: "Bici3", weight: 5 },
+  { name: "Neri", weight: 8 },
+  { name: "Rossi", weight: 7 },
+  { name: "Bianchi", weight: 5 },
 ];
 
 let minorWeigthBike = bicycleArray[0];
@@ -19,3 +21,15 @@ const { name, weight } = minorWeigthBike;
 console.log(
   `La bicicicletta con peso minore è ${name} con un peso di ${weight} kg.`
 );
+
+cardHtml = `
+<div class="card">
+    <img src="https://www.bicidastrada.it/wp-content/uploads/2018/07/Oltre-XR4-disc-3-4.jpg"
+    class="card-img-top" alt="..."/>
+    <div class="card-body">
+       <h5 class="card-title">${name}</h5>
+       <p class="card-text">La bicicletta pesa solo ${weight}Kg</p>
+    </div>
+</div>`;
+
+cardContainerEl.innerHTML = cardHtml;
