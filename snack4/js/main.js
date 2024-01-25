ec07ad5af0ae179dc1048555248f87b028279d4c
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-25 15:28:01 +0100

    feat: svolgimento

--- snack4/js/main.js
+++ snack4/js/main.js
@@ -1,6 +1,36 @@
-console.log("js ok");
+const tableBodyEl = document.getElementById("table-body");
 
-const resultElement = document.getElementById("results");
+const teams = [
+  { name: "Team Turtle", score: 0, foul: 0 },
+  { name: "Team Frog", score: 0, foul: 0 },
+  { name: "Team Penguin", score: 0, foul: 0 },
+  { name: "Team Hippopotamus", score: 0, foul: 0 },
+  { name: "Team Seal", score: 0, foul: 0 },
+  { name: "Team Crocodile", score: 0, foul: 0 },
+  { name: "Team Dolphin", score: 0, foul: 0 },
+];
+console.table(teams);
 
-getRandomNumber(1, 20);
-console.log(randomNumber);
+const Randomizer = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const teamsFoul = teams.map((team, index) => {
+  let { name, score, foul } = team;
+  score = Randomizer(1, 100);
+  foul = Randomizer(1, 100);
+  console.log(name, foul);
+  return { name, foul };
+});
+console.table(teamsFoul);
+
+teamsFoul.forEach((team, index) => {
+  const { name, foul } = team;
+  let tableRowHtml = `
+    <tr>
+      <th scope="row">${index + 1}</th>
+      <td>${name}</td>
+      <td>${foul}</td>
+    </tr>`;
+
+  tableBodyEl.innerHTML += tableRowHtml;
+});
